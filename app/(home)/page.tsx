a26d90c07a4518431bdfa474604cccb4b3b9020a
Giovanni <giovanni.chitarrelli@gmail.com>
2024-02-01 18:01:20 -0300

    feat: add initial header component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Header from '../_components/header';
+
+export default function Home() {
+  return (
+
+    <div>
+      <Header />
+    </div>
+  );
+}
