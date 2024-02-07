d87fba79e7580b9f502e9e96ee54560898d6a209
Giovanni <giovanni.chitarrelli@gmail.com>
2024-02-06 21:16:47 -0300

    feat: add sign and sign out

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,27 +3,95 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
-import { signIn, useSession } from "next-auth/react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon, MenuIcon, UserIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 
 const Header = () => {
 
-    // const { data } = useSession();
-    const handleLoginClick = async () => {
-        await signIn();
+    const { data } = useSession();
+
+    const handleLogoutClick = () => {
+        signOut();
+    }
+
+    const handleLoginClick = () => {
+        signIn("google");
     }
 
     return (
         <Card  >
             <CardContent className="p-5 justify-between items-center flex flex-row" >
                 <Image src="/logo.png" alt="barber" height={22} width={120} />
-                {/* <Button variant="outline" size="icon">
-                    <MenuIcon size={18}/>
-                </Button> */}
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <MenuIcon size={18} />
+                        </Button>
+                    </SheetTrigger>
 
-                <Button onClick={handleLoginClick}>Login</Button>
-                {/* {data?.user ? <h1>{data.user.name}</h1> : <Button onClick={handleLoginClick}>Login</Button>} */}
+                    <SheetContent className="p-0">
+                        <SheetHeader className="p-5 text-left border-b border-solid border-secondary">
+                            <SheetTitle>Menu</SheetTitle>
+                        </SheetHeader>
+
+
+                        {data?.user ? (
+                            <div className="flex justify-between px-5 py-6 items-center">
+                                <div className="flex items-center gap-3 ">
+                                    <Avatar>
+                                        <AvatarImage src={data.user?.image ?? ""} />
+                                    </Avatar>
+
+                                    <h2 className="font-bold">{data.user.name}</h2>
+                                </div>
+
+                                <Button variant="secondary" size="icon">
+                                    <LogOutIcon onClick={handleLogoutClick} />
+                                </Button>
+                            </div>
+
+                        ) : (
+                            <div className="flex flex-col px-5 py-6 gap-3">
+                                <div className="flex items-center gap-2 ">
+                                    <UserIcon size={32} />
+                                    <h2 className="font-bold">Olá, faça seu login!</h2>
+                                </div>
+
+                                <Button variant="secondary" className="w-full justify-start" onClick={handleLoginClick}>
+                                    <LogInIcon className="mr-2" size={18} />
+                                    Fazer Login
+                                </Button>
+                            </div>
+
+                        )}
+
+                        <div className="flex flex-col gap-3 px-5 ">
+
+                            <Button variant="outline" className="justify-start" asChild>
+                                <Link href="/">
+                                    <HomeIcon size={18} className="mr-2" />
+                                    Inicio
+                                </Link>
+                            </Button>
+
+                            {data?.user && (
+
+                                <Button variant="outline" className="justify-start" asChild>
+                                    <Link href="/bookings">
+                                        <CalendarIcon size={18} className="mr-2" />
+                                        Agendamentos
+                                    </Link>
+                                </Button>
+
+                            )}
+                        </div>
+                    </SheetContent>
+                </Sheet>
+
 
             </CardContent>
         </Card>
